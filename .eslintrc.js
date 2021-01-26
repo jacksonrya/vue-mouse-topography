5922ac28b5340983fdfd859677f65a5ade8d01e8
Jackson Baker Ryan <jackson.baker.ryan@gmail.com>
2021-01-26 12:30:36 -0800

    Add custom eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,10 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
-    'eslint:recommended'
+    // 'eslint:recommended',
+    '@jacksonrya/eslint-config-standard',
   ],
   parserOptions: {
     parser: 'babel-eslint'
