b9789da46dcb47b1821c755feda5f79a9d683f30
Jackson Baker Ryan <jackson.baker.ryan@gmail.com>
2021-01-27 16:01:35 -0800

    Add UMD build output. point entry to UMD build

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -3,32 +3,50 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import css from 'rollup-plugin-css-only';
 import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
 
-export default [ {
-  input: 'src/wrapper.js', // Path relative to package.json
-  output: {
-    format: 'esm',
-    file: 'dist/vue-mouse-topography.esm.js',
-    name: 'VueMouseTopography',
-    exports: 'named',
+export default [ 
+  {
+    input: 'src/wrapper.js', // Path relative to package.json
+    output: {
+      format: 'umd',
+      file: 'dist/vue-mouse-topography.js',
+      name: 'VueMouseTopography',
+      exports: 'named',
+    },
+    plugins: [
+      css({ output: 'vue-mouse-topography.css' }),
+      vue({ css: false }),
+      nodeResolve(),
+      commonjs(),
+    ],
   },
-  plugins: [
-    css({ output: 'vue-mouse-topography.esm.css' }),
-    vue({ css: false }),
-    nodeResolve(),
-    commonjs(),
-  ],
-}, {
-  input: 'src/wrapper.js', // Path relative to package.json
-  output: {
-    format: 'iife',
-    file: 'dist/vue-mouse-topography.min.js',
-    name: 'VueMouseTopography',
-    exports: 'named',
+  {
+    input: 'src/wrapper.js', // Path relative to package.json
+    output: {
+      format: 'esm',
+      file: 'dist/vue-mouse-topography.esm.js',
+      name: 'VueMouseTopography',
+      exports: 'named',
+    },
+    plugins: [
+      css({ output: 'vue-mouse-topography.esm.css' }),
+      vue({ css: false }),
+      nodeResolve(),
+      commonjs(),
+    ],
   },
-  plugins: [
-    css({ output: 'vue-mouse-topography.min.css' }),
-    vue({ css: false }),
-    nodeResolve(),
-    commonjs(),
-  ],
-} ]
+  {
+    input: 'src/wrapper.js', // Path relative to package.json
+    output: {
+      format: 'iife',
+      file: 'dist/vue-mouse-topography.min.js',
+      name: 'VueMouseTopography',
+      exports: 'named',
+    },
+    plugins: [
+      css({ output: 'vue-mouse-topography.min.css' }),
+      vue({ css: false }),
+      nodeResolve(),
+      commonjs(),
+    ],
+  }, 
+]
