a0c81d218fffc4e9f8fbb94a2481ee5d6b09884d
Jackson Baker Ryan <jackson.baker.ryan@gmail.com>
2021-01-29 13:00:36 -0800

    Remove goldstein distribution preset

--- src/Contours.js
+++ src/Contours.js
@@ -247,26 +247,6 @@ export class Contours {
 
         return values
       },
-      goldstein: () => {
-        const values = initValues()
-
-        function goldsteinPrice (x, y) {
-          return (1 + Math.pow(x + y + 1, 2) * (19 - 14 * x + 3 * x * x - 14 * y + 6 * x * x + 3 * y * y)) *
-            (30 + Math.pow(2 * x - 3 * y, 2) * (18 - 32 * x + 12 * x * x + 48 * y - 36 * x * y + 27 * y * y))
-        }
-
-        // Populate a grid of n×m values where -2 ≤ x ≤ 2 and -2 ≤ y ≤ 1.
-        const n = this.resolution.width
-        const m = this.resolution.height
-        for (let j = 0.5, k = 0; j < m; ++j) {
-          for (let i = 0.5; i < n; ++i, ++k) {
-            values[k] = goldsteinPrice(i / n * 4 - 2, 1 - j / m * 3)
-          }
-        }
-
-        return values
-      },
-
     }
 
     matrices.empty = initValues // initValues
