b62181a93bf52febfbfa4ef9fd2ce6294dae444e
Jackson Baker Ryan <jackson.baker.ryan@gmail.com>
2023-04-28 01:18:35 -0700

    Remove babel, update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
   'root': true,
-  'env': { 'node': true },
+  'env': {
+    'node': true,
+    'es2022': true,
+  },
   'extends': [
     'plugin:vue/recommended',
     'eslint:recommended',
     '@jacksonrya/eslint-config-standard', 
   ],
-  'parserOptions': { 'parser': 'babel-eslint' },
   'rules': {
     'no-debugger': 1,
     'space-infix-ops': 2,
